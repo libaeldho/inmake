f22157904f5695cebfaa2acf5bffa473614594bd
libaeldho <libaeldho9@gmail.com>
2023-01-22 00:33:16 -1200

    save

---
+++ filmproject/film/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+
+from film import views
+app_name = 'film'
+urlpatterns = [
+    path('', views.film, name='film'),
+    path('film/<int:film_id>/', views.detail, name='detail'),
+    path('add/', views.add_film, name='add_film'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('delete/<int:id>/', views.delete, name='delete'),
+
+]
